e3e0c02b1d4732da2616d8424c2779fb34d4ff45
E1ri <hokk3dverum@gmail.com>
2023-02-13 03:27:41 +0300

    favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <title>e1ri</title>
         <meta name="description" content="e1ri's homepage" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <div>
         <DevPlaceholder />
