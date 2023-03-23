52d1897de8de1bce375a26a64c406ead47953289
E1ri <hokk3dverum@gmail.com>
2023-03-23 23:49:40 +0300

    game update

--- src/components/Nanogram/Nanogram.tsx
+++ src/components/Nanogram/Nanogram.tsx
@@ -1,32 +1,87 @@
 import { clueInfo, nanoGen } from "@/utils/nanogram";
+import classNames from "classnames";
 import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 export const Nanogram = () => {
-  const [nanoArr, setNanoArr] = useState<number[][] | null>(null);
-  const [clues, setClues] = useState<number[][] | null>(null);
+  const [nanoArr, setNanoArr] = useState<any[][] | null>(null);
+  const [cluesHoriz, setCluesHoriz] = useState<number[][] | null>(null);
+  const [cluesVert, setCluesVert] = useState<number[][] | null>(null);
+  const [isWin, setIsWin] = useState<boolean>(false);
+
   useEffect(() => {
     const arr = nanoGen(10);
+    const { rows, columns } = clueInfo(arr);
     setNanoArr(arr);
-    setClues(clueInfo(arr));
-    console.log(clueInfo(arr));
+    setCluesHoriz(rows);
+    setCluesVert(columns);
   }, []);
+
+  const changeStatusHandler = (indexRow: number, elIndex: number, el: any) => {
+    if (nanoArr === null) {
+      return;
+    }
+    if (el.content === 0 || el.isPressed === true) {
+      return;
+    }
+    const changed = [...nanoArr];
+    changed[indexRow][elIndex].isPressed = true;
+    setNanoArr(changed);
+    gameOverChecker(changed);
+    console.log(changed);
+  };
+
+  const gameOverChecker = (grid: any[][]) => {
+    for (let x of grid) {
+      for (let y of x) {
+        if (y.content === 1 && y.isPressed !== true) {
+          return;
+        }
+      }
+    }
+    setIsWin(true);
+  };
 
   return (
     <div className={styles.nano}>
+      <div className={styles.nano__top_row}>
+        {cluesVert !== null &&
+          cluesVert.map((el, index) => {
+            return (
+              <div className={styles.nano__clue_row} key={index}>
+                {el.map((el, index) => {
+                  return (
+                    <div key={index} className={styles.nano__clue_cell}>
+                      {el}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+      </div>
       {nanoArr !== null &&
         nanoArr.map((el, indexRow) => {
           return (
             <div className={styles.nano__row} key={indexRow}>
-              {clues !== null &&
-                clues[indexRow].map((el, index) => {
-                  return <div key={index}>{el}</div>;
+              {cluesHoriz !== null &&
+                cluesHoriz[indexRow].map((el, index) => {
+                  return (
+                    <div className={styles.nano__clue_cell} key={index}>
+                      {el}
+                    </div>
+                  );
                 })}
               {el.map((cell, index) => {
                 return (
-                  <div className={styles.nano__cell} key={index}>
-                    {cell}
-                  </div>
+                  <div
+                    className={classNames({
+                      [styles.nano__cell]: true,
+                      [styles.nano__cell_correct]: cell.isPressed,
+                    })}
+                    onClick={() => changeStatusHandler(indexRow, index, cell)}
+                    key={index}
+                  />
                 );
               })}
             </div>
--- src/utils/nanogram.ts
+++ src/utils/nanogram.ts
@@ -1,13 +1,19 @@
 export const nanoGen = (size: number): number[][] => {
-  const rows: number[][] = [];
-  const columns: number[][] = [];
+  const rows: any[][] = [];
+  const columns: any[][] = [];
 
   for (let i = 0; i < size; i++) {
     const row = [];
     const column = [];
     for (let j = 0; j < Math.floor(size / 2); j++) {
-      const randomRow = Math.round(Math.random());
-      const randomColumn = Math.round(Math.random());
+      const randomRow = {
+        content: Math.round(Math.random()),
+        isPressed: false,
+      };
+      const randomColumn = {
+        content: Math.round(Math.random()),
+        isPressed: false,
+      };
       row.push(randomRow);
       column.push(randomColumn);
     }
@@ -15,25 +21,31 @@ export const nanoGen = (size: number): number[][] => {
     columns.push(column);
   }
 
-  const grid: number[][] = [];
+  const grid: any[][] = [];
 
   for (let i = 0; i < size; i++) {
-    const row = new Array(size).fill(0);
+    const row = new Array(size).fill({ content: 0, isPressed: false });
     grid.push(row);
   }
 
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < rows[i].length; j++) {
       const index = size - j - 1;
-      if (rows[i][j] === 1) {
-        grid[i][index] = 1;
+      if (rows[i][j].content === 1) {
+        grid[i][index] = {
+          content: 1,
+          isPressed: false,
+        };
       }
     }
 
     for (let j = 0; j < columns[i].length; j++) {
       const index = size - j - 1;
-      if (columns[i][j] === 1) {
-        grid[index][i] = 1;
+      if (columns[i][j].content === 1) {
+        grid[index][i] = {
+          content: 1,
+          isPressed: false,
+        };
       }
     }
   }
@@ -41,15 +53,18 @@ export const nanoGen = (size: number): number[][] => {
   return grid;
 };
 
-export const clueInfo = (grid: number[][]): any[][] => {
+export const clueInfo = (grid: any[][]): any => {
   const rows: any[][] = [];
-  const columns = [];
+  const columns: any[][] = [];
+  let currentIndex = 0;
 
   for (let x of grid) {
     let repeatCount = 0;
+    let repeatCountHoriz = 0;
+    const column: any[] = [];
     const row: any[] = [];
     for (let y of x) {
-      if (y === 1) {
+      if (y.content === 1) {
         repeatCount++;
       } else {
         if (repeatCount > 0) {
@@ -58,12 +73,31 @@ export const clueInfo = (grid: number[][]): any[][] => {
         repeatCount = 0;
       }
     }
+
+    for (let num = 0; num <= grid.length - 1; num++) {
+      const el = grid[num][currentIndex];
+      if (el.content === 1) {
+        repeatCountHoriz++;
+      } else {
+        if (repeatCountHoriz > 0) {
+          column.push(repeatCountHoriz);
+        }
+        repeatCountHoriz = 0;
+      }
+    }
+
     if (repeatCount > 0) {
       row.push(repeatCount);
     }
+    if (repeatCountHoriz > 0) {
+      column.push(repeatCountHoriz);
+    }
+    repeatCountHoriz = 0;
     repeatCount = 0;
     rows.push(row);
+    columns.push(column);
+    currentIndex++;
   }
 
-  return rows;
+  return { rows, columns };
 };
