747e4f39ba5c3a8d2cf24682398495d2c148b94b
E1ri <hokk3dverum@gmail.com>
2023-04-03 16:54:43 +0300

    position and mistake counter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,19 @@ import styles from "../styles/index.module.scss";
 
 const Home = () => {
   const [isWin, setIsWin] = useState<boolean>(false);
+  const [mistakesCount, setMistakesCount] = useState<number>(0);
   return (
     <>
       <Head>
-        <title>e1ri</title>
+        <title>E1RI</title>
         <meta name="description" content="e1ri's homepage" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" />
       </Head>
       <div className={styles.main}>
-        {isWin && <div className={styles.main__text}>WINWINWINWIN</div>}
-        <Nanogram setIsWin={setIsWin} />
+        <div className={styles.main__text}>Mistakes: {mistakesCount}</div>
+        {isWin && <div className={styles.main__text}>WINWINWIN</div>}
+        <Nanogram setMistakesCount={setMistakesCount} setIsWin={setIsWin} />
       </div>
     </>
   );
